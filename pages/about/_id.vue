0033bfabcd52c8282f827f4fd8b6b857f43ce95b
Still_Learning <smslam2999@gmail.com>
2018-06-05 11:39:35 +0600

    Add files via upload

---
+++ pages/about/_id.vue
@@ -0,0 +1,6 @@
+<template>
+<div>
+    <h1>this is about page with id: {{$route.params.id}}</h1>
+    <router-link to="/">buckh to howm</router-link>
+</div>
+</template>
---
+++ pages/about/index.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+    <h1>aa baa out</h1>
+    <router-link to="/">buckh to howm</router-link>
+     <router-link to="/_id">to about with id</router-link>
+</div>
+</template>
